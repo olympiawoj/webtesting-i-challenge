e479670c7cde9be66cdb4721f3bc28807864f753
olympia <owojcik7@gmail.com>
2019-04-08 19:32:59 -0400

    Add test for function repair(item)

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,11 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+it("should accept an item object and return a new object w/ durability restored to 100", () => {
+  const item = { name: "item", durability: 0, enhancement: 10 };
+  expect(enhancer.repair(item)).toEqual({
+    name: "item",
+    durability: 100,
+    enhancement: 10
+  });
+});
